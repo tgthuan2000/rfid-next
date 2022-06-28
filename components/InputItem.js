fc67672afbbae102bea07dc08f0702b4af8026cf
thuantg@pitek.one <thuantg@pitek.one>
2022-06-28 12:02:45 +0700

    commit

--- components/InputItem.js
+++ components/InputItem.js
@@ -1,22 +1,31 @@
-import clsx from 'clsx'
-import { forwardRef } from 'react'
+import clsx from "clsx";
+import { forwardRef } from "react";
 
-const InputItem = forwardRef(({ label = 'Label', className, type = 'text', ...props }, ref) => {
-	return (
-		<div className={clsx('sm:col-span-3', className)}>
-			<label htmlFor={label} className='block text-sm font-medium text-gray-700'>
-				{label}
-			</label>
-			<input
-				ref={ref}
-				id={label}
-				type={type}
-				autoComplete='off'
-				className='mt-1 flex-1 block w-full min-w-0 rounded-md sm:text-sm border-gray-300 disabled:bg-gray-100 disabled:cursor-not-allowed'
-				{...props}
-			/>
-		</div>
-	)
-})
+const InputItem = forwardRef(
+  (
+    { label = "Label", className, type = "text", error, required, ...props },
+    ref
+  ) => {
+    return (
+      <div className={clsx("sm:col-span-3", className)}>
+        <label
+          htmlFor={label}
+          className="block text-sm font-medium text-gray-700"
+        >
+          {label} {required && <span className="text-red-500">*</span>}
+        </label>
+        <input
+          ref={ref}
+          id={label}
+          type={type}
+          autoComplete="off"
+          className="mt-1 flex-1 block w-full min-w-0 rounded-md sm:text-sm border-gray-300 disabled:bg-gray-100 disabled:cursor-not-allowed"
+          {...props}
+        />
+        {error && <span className="text-xs text-red-500">{error}</span>}
+      </div>
+    );
+  }
+);
 
-export default InputItem
+export default InputItem;
--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,146 +1,160 @@
-import { useRouter } from 'next/router'
-import InputItem from './InputItem'
-import TextaereaItem from './TextaereaItem'
-import SelectItem from './SelectItem'
-import AvatarItem from './AvatarItem'
-import { useQuery } from 'hooks/useQuery'
-import { categoryProduct } from 'sanity/query'
-import { Controller, useForm } from 'react-hook-form'
-import { useEffect } from 'react'
-import { client } from 'sanity/client'
+import { useRouter } from "next/router";
+import InputItem from "./InputItem";
+import TextaereaItem from "./TextaereaItem";
+import SelectItem from "./SelectItem";
+import AvatarItem from "./AvatarItem";
+import { useQuery } from "hooks/useQuery";
+import { categoryProduct } from "sanity/query";
+import { Controller, useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { client } from "sanity/client";
 
 const ProductForm = ({ title }) => {
-	const { data: selectData, loading: selectLoading } = useQuery(categoryProduct.GET_ALL)
+  const { data: selectData, loading: selectLoading } = useQuery(
+    categoryProduct.GET_ALL
+  );
 
-	const router = useRouter()
+  const router = useRouter();
 
-	const { control, handleSubmit, setValue } = useForm({
-		defaultValues: {
-			avatar: null,
-			name: '',
-			desc: '',
-			category: null,
-			price: 0,
-			quantity: 0,
-		},
-	})
+  const { control, handleSubmit, setValue } = useForm({
+    defaultValues: {
+      avatar: null,
+      name: "",
+      desc: "",
+      category: null,
+      price: 0,
+      quantity: 0,
+    },
+  });
 
-	useEffect(() => {
-		if (selectData.length > 0) setValue('category', selectData[0]._id)
-	}, [selectData])
+  useEffect(() => {
+    if (selectData.length > 0) setValue("category", selectData[0]._id);
+  }, [selectData]);
 
-	const onSubmit = (data) => {
-		const { avatar, name, desc, category, price, quantity } = data
-		client
-			.create({
-				_type: 'product',
-				categoryProduct: {
-					_ref: category,
-					_type: 'reference',
-				},
-				image: {
-					_type: 'image',
-					asset: {
-						_ref: avatar,
-						_type: 'reference',
-					},
-				},
-				barcode: {
-					_type: 'slug',
-					current: (Math.random() * 1000000000000000).toString().substring(0, 13),
-				},
-				description: desc,
-				name,
-				price: Number(price),
-				quantity: Number(quantity),
-			})
-			.then(() => {
-				router.back()
-			})
-			.catch((error) => console.log(error))
-	}
+  const onSubmit = (data) => {
+    const { avatar, name, desc, category, price, quantity } = data;
+    client
+      .create({
+        _type: "product",
+        categoryProduct: {
+          _ref: category,
+          _type: "reference",
+        },
+        image: {
+          _type: "image",
+          asset: {
+            _ref: avatar,
+            _type: "reference",
+          },
+        },
+        barcode: {
+          _type: "slug",
+          current: (Math.random() * 1000000000000000)
+            .toString()
+            .substring(0, 13),
+        },
+        description: desc,
+        name,
+        price: Number(price),
+        quantity: Number(quantity),
+      })
+      .then(() => {
+        router.back();
+      })
+      .catch((error) => console.log(error));
+  };
 
-	return (
-		<div>
-			<form className='space-y-8 divide-y divide-gray-200' onSubmit={handleSubmit(onSubmit)}>
-				<div>
-					<div className='flex justify-between items-center'>
-						<h1 className='text-3xl font-semibold text-gray-900'>{title}</h1>
-						<div className='mt-4 sm:mt-0 sm:ml-16 sm:flex-none grid gap-2 grid-cols-1 sm:grid-cols-2'>
-							<button
-								onClick={() => router.back()}
-								type='button'
-								className='min-w-[120px] inline-flex font-semibold items-center justify-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm text-white shadow-sm hover:bg-gray-700 sm:w-auto'
-							>
-								Hủy bỏ
-							</button>
-							<button
-								type='submit'
-								className='min-w-[120px] inline-flex font-semibold items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm text-white shadow-sm hover:bg-red-700 sm:w-auto'
-							>
-								Lưu
-							</button>
-						</div>
-					</div>
-					<div className='w-full bg-white px-8 pt-8 pb-20 rounded-lg mt-6'>
-						<div className='max-w-4xl mx-auto grid grid-cols-1 gap-y-6 lg:gap-x-10 md:gap-x-8 sm:gap-x-6 sm:grid-cols-6'>
-							<Controller
-								control={control}
-								name='avatar'
-								render={({ field }) => (
-									<AvatarItem label='Hình ảnh' onChange={field.onChange} />
-								)}
-							/>
+  return (
+    <div>
+      <form
+        className="space-y-8 divide-y divide-gray-200"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div>
+          <div className="flex justify-between items-center">
+            <h1 className="text-3xl font-semibold text-gray-900">{title}</h1>
+            <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none grid gap-2 grid-cols-1 sm:grid-cols-2">
+              <button
+                onClick={() => router.back()}
+                type="button"
+                className="min-w-[120px] inline-flex font-semibold items-center justify-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm text-white shadow-sm hover:bg-gray-700 sm:w-auto"
+              >
+                Hủy bỏ
+              </button>
+              <button
+                type="submit"
+                className="min-w-[120px] inline-flex font-semibold items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm text-white shadow-sm hover:bg-red-700 sm:w-auto"
+              >
+                Lưu
+              </button>
+            </div>
+          </div>
+          <div className="w-full bg-white px-8 pt-8 pb-20 rounded-lg mt-6">
+            <div className="max-w-4xl mx-auto grid grid-cols-1 gap-y-6 lg:gap-x-10 md:gap-x-8 sm:gap-x-6 sm:grid-cols-6">
+              <Controller
+                control={control}
+                name="avatar"
+                render={({ field }) => (
+                  <AvatarItem label="Hình ảnh" onChange={field.onChange} />
+                )}
+              />
 
-							<Controller
-								control={control}
-								name='name'
-								rules={{ required: 'Yêu cầu nhập tên' }}
-								render={({ field }) => (
-									<InputItem label='Tên sản phẩm' {...field} />
-								)}
-							/>
+              <Controller
+                control={control}
+                name="name"
+                rules={{ required: "Yêu cầu nhập tên." }}
+                render={({ field, fieldState: { error } }) => (
+                  <InputItem
+                    label="Tên sản phẩm"
+                    required
+                    error={error?.message}
+                    {...field}
+                  />
+                )}
+              />
 
-							<Controller
-								control={control}
-								name='category'
-								render={({ field }) => (
-									<SelectItem
-										label='Loại sản phẩm'
-										data={selectData}
-										loading={selectLoading}
-										{...field}
-									/>
-								)}
-							/>
+              <Controller
+                control={control}
+                name="category"
+                render={({ field }) => (
+                  <SelectItem
+                    label="Loại sản phẩm"
+                    data={selectData}
+                    loading={selectLoading}
+                    {...field}
+                  />
+                )}
+              />
 
-							<Controller
-								control={control}
-								name='price'
-								render={({ field }) => (
-									<InputItem label='Giá sản phẩm' type='number' {...field} />
-								)}
-							/>
+              <Controller
+                control={control}
+                name="price"
+                render={({ field }) => (
+                  <InputItem label="Giá sản phẩm" type="number" {...field} />
+                )}
+              />
 
-							<Controller
-								control={control}
-								name='quantity'
-								render={({ field }) => (
-									<InputItem label='Số lượng tồn' type='number' {...field} />
-								)}
-							/>
+              <Controller
+                control={control}
+                name="quantity"
+                render={({ field }) => (
+                  <InputItem label="Số lượng tồn" type="number" {...field} />
+                )}
+              />
 
-							<Controller
-								control={control}
-								name='desc'
-								render={({ field }) => <TextaereaItem label='Mô tả' {...field} />}
-							/>
-						</div>
-					</div>
-				</div>
-			</form>
-		</div>
-	)
-}
+              <Controller
+                control={control}
+                name="desc"
+                render={({ field }) => (
+                  <TextaereaItem label="Mô tả" {...field} />
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default ProductForm
+export default ProductForm;
